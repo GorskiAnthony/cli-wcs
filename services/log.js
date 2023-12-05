598c0862d9f226e9be68533bc335b4c90a724c06
Anthony Gorski <anthony.developpeurweb@gmail.com>
2023-12-05 19:18:43 +0100

    update packaga.json

---
+++ services/log.js
@@ -0,0 +1,16 @@
+const chalk = require("chalk");
+
+// success
+function success(message) {
+	console.log(chalk.greenBright(message));
+}
+
+// error
+function error(message) {
+	console.log(chalk.redBright(message));
+}
+
+module.exports = {
+	success,
+	error,
+};
